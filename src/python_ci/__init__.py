0ae780c076fe2dc9102c35b65f80321955243f72
Peyman <hassani.peyman627@gmail.com>
2025-06-14 11:07:21 +0330

    Setup github actions

---
+++ src/python_ci/__init__.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("Hello from python-ci!")
+
+
+def generate_number() -> int:
+    return 26
---
+++ tests/python_ci/test_init.py
@@ -0,0 +1,5 @@
+from python_ci import generate_number
+
+
+def test_generate_number():
+    assert generate_number() == 26
